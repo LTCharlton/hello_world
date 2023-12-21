5997413888c79cd17bcfa566bbd9d770ad77b8ab
Lindsey Charlton <linser22@gmail.com>
2023-12-21 11:17:57 +0000

    Playing around with pointers, I still dont have any real use for this repo

--- main.cpp
+++ main.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 
 int num;
+struct shirt{
+        string color;
+        int count;
+
+};
 int main()
 {
-    cout << "Hello";
+
+   
+    //Its been awhile since I last played around with pointers in C++ so I'm just playing around with it
+    shirt *graphic_t = new shirt;
+    graphic_t->color = "blue";
+    graphic_t->count = 5;
+
+    char *test = new char;
+    cin >> test;
+    cout << graphic_t->color << endl;
     cin >> num;
 return 0;
 }
