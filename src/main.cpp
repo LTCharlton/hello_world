c5b88c0996ab0b37eaeafbbc2fef5a657d28dce0
Lindsey Charlton <linser22@gmail.com>
2023-12-27 11:19:00 +0000

    starting the groundwork for a tool that can manipulate strings, for fun

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 using namespace std;
-
+#include "string_tools.h"
 
 int num;
 struct shirt{
@@ -12,15 +12,17 @@ struct shirt{
 };
 int main()
 {
+    string_tools test;
 
+    test.doStuff();
    
     //Its been awhile since I last played around with pointers in C++ so I'm just playing around with it
     shirt *graphic_t = new shirt;
     graphic_t->color = "blue";
     graphic_t->count = 5;
 
-    char *test = new char;
-    cin >> test;
+    char *ctest = new char;
+    cin >> ctest;
     cout << graphic_t->color << endl;
     cin >> num;
 return 0;
---
+++ src/string_tools.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class string_tools{
+public:
+int taco = 0;
+
+void doStuff();
+
+};
+
+void string_tools::doStuff(){
+    cout << "yo whatsup!";
+}
